3d1bf7e800556b4d54daa236418464ceb37a470b
Katie <Katieau.x@gmail.com>
2024-02-28 23:28:51 -0800

    Finished twentyOne project

--- CodingChallenges/CodingChallenges.cs
+++ CodingChallenges/CodingChallenges.cs
@@ -1,9 +1,54 @@
 ﻿namespace Assignment1
 {
+
+    class Vector2
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+    class Character
+    {
+        public Vector2 position { get; set; }
+        public string name;
+        public Vector2 translateDistance { get; set; }
+
+        public Character(Vector2 Position, string Name)
+        {
+            Position = position;
+            Name = name;
+        }
+
+        public Vector2 Translate(Vector2 TranslateDistance,Vector2 position)
+        {
+            Vector2 NewLocation = new Vector2(position.X, position.Y);
+            NewLocation.X = NewLocation.X + translateDistance.X;
+            NewLocation.Y = NewLocation.Y + translateDistance.Y;
+            return NewLocation;         
+        }
+    }
+
+    
     internal class GuessingGame
     {
         static void Main(string[] args)
         {
+            Vector2 location = new Vector2(0.0f, 0.0f);
+
+            Character character = new Character(location, "cube");
+            character.translateDistance = new Vector2(1.5f, 2.5f);
+            character.position = character.Translate(character.translateDistance, character.position);
+            Console.WriteLine(character.position);
+            
+           
+
+
             //        Console.WriteLine("Enter the Maximum number");
             //        int num = Convert.ToInt32(Console.ReadLine());
             //        Random rand = new Random();
@@ -36,49 +81,51 @@
             //    }
             //}
 
-            string text = "The quick brown fox jumps over the lazy dog";
-            bool run = true;
-            int loop = 0;
-            while (run)
-            {
-                if (loop == 0)
-                {
-                    Console.Clear();
-                    for (int i = 0; i < text.Length; i++)
-                    {
-                        Console.Write(text[i]);
-                        Thread.Sleep(100);
-                    }
-                    Console.WriteLine();
+            //string text = "The quick brown fox jumps over the lazy dog";
+            //bool run = true;
+            //int loop = 0;
+            //while (run)
+            //{
+            //    if (loop == 0)
+            //    {
+            //        Console.Clear();
+            //        for (int i = 0; i < text.Length; i++)
+            //        {
+            //            Console.Write(text[i]);
+            //            Thread.Sleep(100);
+            //        }
+            //        Console.WriteLine();
 
-                    Console.WriteLine("Enter a letter");
-                    loop++;
-                }
+            //        Console.WriteLine("Enter a letter");
+            //        loop++;
+            //    }
 
-                else
-                {
-                    string letterString = Console.ReadLine();
-                    char LetterChar = Convert.ToChar(letterString);
-                    Console.Clear();
-                    for (int i = 0; i < text.Length; i++)
-                    {
-                        if (text[i] == LetterChar)
-                        {
-                            Console.Write(char.ToUpper(text[i]));
-                            Thread.Sleep(100);
-                        }
+            //    else
+            //    {
+            //        string letterString = Console.ReadLine();
+            //        char LetterChar = Convert.ToChar(letterString);
+            //        Console.Clear();
+            //        for (int i = 0; i < text.Length; i++)
+            //        {
+            //            if (text[i] == LetterChar)
+            //            {
+            //                Console.Write(char.ToUpper(text[i]));
+            //                Thread.Sleep(100);
+            //            }
 
-                        else
-                        {
-                            Console.Write(text[i]);
-                            Thread.Sleep(100);
-                        }
-                    }
-                    Console.WriteLine();
+            //            else
+            //            {
+            //                Console.Write(text[i]);
+            //                Thread.Sleep(100);
+            //            }
+            //        }
+            //        Console.WriteLine();
 
-                    Console.WriteLine("Enter a letter");
-                }
-            }
+            //        Console.WriteLine("Enter a letter");
+            //    }
+            //}
+
+
 
                
             }
