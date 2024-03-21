33dc821f6cd93e1a1000c1b625a89e15641e70b8
VFS\var06katie <Katieau.x@gmail.com>
2024-03-21 15:31:05 -0700

    Started Chess assignment and created class Knight

---
+++ Chess/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Chess
+{
+    abstract class Piece
+    {
+        public Vector2 Pos;
+        public char Glyph;
+        public Func<List<Vector2>> GetMovements;
+
+        public Piece(char glyph)
+        {
+            Glyph = glyph;
+        }
+
+        public abstract List<Vector2> CalculateMovements();
+    }
+
+    class Knight : Piece
+    {
+        public Knight(char glyph) : base(glyph) { }
+
+        public override List<Vector2> CalculateMovements()
+        {
+            List<Vector2> movements = [
+                Pos + new Vector2(1, -2),
+                Pos - new Vector2(2, -1),
+                Pos + new Vector2(2, -1),
+                Pos - new Vector2(1, -2),
+                Pos + new Vector2(1, 2),
+                Pos - new Vector2(2, 1),
+                Pos + new Vector2(2, 1),
+                Pos - new Vector2(1, 2)
+            ];
+            return movements;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
